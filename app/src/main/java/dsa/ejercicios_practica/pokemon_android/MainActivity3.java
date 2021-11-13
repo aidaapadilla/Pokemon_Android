537e69d32c739afc999671607b3d40a0898bb8de
Aidaapadilla <aida.padilla.gonzalez.aiko@gmail.com>
2021-11-13 14:04:53 +0100

    Main

--- app/src/main/java/dsa/ejercicios_practica/pokemon_android/MainActivity3.java
+++ app/src/main/java/dsa/ejercicios_practica/pokemon_android/MainActivity3.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.LogIn);
+        setContentView(R.layout.activity_main2);
     }
-
 }
