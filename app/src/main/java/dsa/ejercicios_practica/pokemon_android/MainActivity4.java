0d548f0314b83b4150bde0742bc88187779af1c4
Aidaapadilla <aida.padilla.gonzalez.aiko@gmail.com>
2021-11-15 11:45:37 +0100

    Creant el recyclerview LES COSES DEL ITEM SON TRANSPARENTS NO BORRAR PORFA!

--- app/src/main/java/dsa/ejercicios_practica/pokemon_android/MainActivity4.java
+++ app/src/main/java/dsa/ejercicios_practica/pokemon_android/MainActivity4.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity3 extends AppCompatActivity {
+public class MainActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Perfil);
+        setContentView(R.layout.activity_main);
+
     }
 }
